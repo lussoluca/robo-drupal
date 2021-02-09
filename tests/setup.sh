2b74457d91cef350639d2c58abf86787dc6eb6ca
Luca Cracco <craccoluca@gmail.com>
2021-02-09 19:26:23 +0100

    Init procedure for test the robo commands

---
+++ tests/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit when any command fails.
+set -e
+
+if ! command -v composer &> /dev/null
+then
+  echo "Composer executable could not be found"
+  exit
+fi
+
+echo -e "\n\n\nClear/Create directory of install drupal for tests"
+mkdir -p drupal
+rm -rf drupal/*
+
+DRUPAL_CORE_CONSTRAINT="${DRUPAL_CORE_CONSTRAINT:=^8.9}"
+echo -e "\n\n\nInstalling composer"
+cp composer.dev.json drupal/composer.json
+cd drupal
+composer require --dev --no-interaction composer/installers:^1
+# Accept a constraint for Drupal core version.
+composer require --dev --no-update drupal/core-recommended:$DRUPAL_CORE_CONSTRAINT drupal/core-dev:$DRUPAL_CORE_CONSTRAINT drupal/core-composer-scaffold:$DRUPAL_CORE_CONSTRAINT drupal/core:$DRUPAL_CORE_CONSTRAINT
+echo -e "\nInstalling dependencies"
+composer update --dev --no-interaction
